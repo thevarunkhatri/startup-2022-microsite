6db47c88a9cbe8b5be807d5d680e96a66faf9a18
thevarunkhatri <varunmk05@gmail.com>
2021-11-24 14:02:07 -0500

    base structure and styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
 import React from "react"
+import '../styles/styles.scss'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return(
+    <div className="fullPage">
+      <div className="box">
+        <header>
+          <p>A competition for innovation and growth</p>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+          <ul className="links">
+            <a href=""><li>Startup 2021</li></a>
+            <a href=""><li>Startup Instagram</li></a>
+            <a href=""><li>FLUX Instagram</li></a>
+            <a href=""><li>Contact Us</li></a>
+          </ul>
+          <p>
+            SCAD StartUp is our annual, week-long design sprint hosted by FLUX - the UX club in collaboration with SCADpro
+          </p>
+        </footer>
+      </div>
+    </div>
+  ) 
 }
